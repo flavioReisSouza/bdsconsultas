b504e71fa0a4ca99eadb5ddb49e4e1d75abeecdc
Flavio Reis <flavioreissouza@yahoo.com>
2023-08-15 16:04:46 -0300

    Created Projection and DTO

--- uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/repositories/CustomerRepository.java
@@ -1,4 +1,16 @@
 package com.devsuperior.uri2602.repositories;
 
-public class CustomerRepository {
+import com.devsuperior.uri2602.entities.Customer;
+import com.devsuperior.uri2602.projections.CustomerMinProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+//Vamos definir uma projeção, ou seja, limitar os campos da tabela que serão retornados.
+// Somente os campos que eu preciso.
+public interface CustomerRepository extends JpaRepository<Customer, Long> { // JpaRepository<Entidade, Tipo da chave primária da entidade>
+
+    @Query(nativeQuery = true, value = "SELECT name FROM customers WHERE UPPER(state) = UPPER(:state)")
+    List<CustomerMinProjection> search1(String state);
 }
